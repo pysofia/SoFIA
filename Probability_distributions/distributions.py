d2e7bea8f83b9eda9a01692193edce6b8886738d
Anabel del Val <ana.isabel.del.val.benitez@vki.ac.be>
2021-07-19 14:31:11 +0200

    commit

--- Probability_distributions/distributions.py
+++ Probability_distributions/distributions.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import special
+from scipy.stats import norm
+import seaborn as sns
 
 class Gaussian:
 
@@ -14,6 +17,7 @@ class Gaussian:
             self.mu[i] = self.hyperparams[i][0]
             self.sigma[i] = self.hyperparams[i][1]
 
+    # PDF
     def get_one_pdf_value(self,x,pos): 
         return np.divide(1,np.sqrt(2*3.1416)*self.sigma[pos])*np.exp(-np.power(np.abs(self.mu[pos] - x),2)/(2*np.power(self.sigma[pos],2)))
 
@@ -21,6 +25,32 @@ class Gaussian:
         values = [0.]*len(x)
         for i in range(len(x)):
             values[i] += self.get_one_pdf_value(x[i],pos)
+        return values
+
+    # CDF
+    def get_one_cdf_value(self,x,pos): 
+        return 0.5*(1+special.erf(np.divide(x-self.mu[pos],self.sigma[pos]*np.sqrt(2))))
+
+    def get_cdf_values(self,x,pos):
+        values = [0.]*len(x)
+        for i in range(len(x)):
+            values[i] += self.get_one_cdf_value(x[i],pos)
+        return values
+
+    # Inverse CDF
+    def inv_cdf(self,x,pos):
+        return self.sigma[pos]*np.sqrt(2)*special.erfinv((2*x-1))+self.mu[pos]
+
+    # Generate random samples
+    def get_one_sample(self,pos):
+        u = np.random.random()
+        return self.inv_cdf(u,pos)
+
+    def get_samples(self,pos,nsamples):
+        values = [0.]*nsamples
+        for i in range(nsamples):
+            values[i] = self.get_one_sample(pos)
+
         return values
 
 class Uniform:
@@ -77,16 +107,22 @@ class Uniform:
 #             values[i] = self.get_one_pdf_value(x[i],pos)
 #         return values
 
-hyp = [[0.0001,1.]]
-Ps = LogUniform(1,hyp)
+hyp = [[8.,0.2]]
+h = [[0.,5.]]
+
+Ps = Gaussian(1,hyp)
+P = Gaussian(1,h)
 
 x = np.linspace(0.00001,10.,1000)
-y = np.linspace(0.,8.,1000)
+y = np.linspace(-8.,8.,1000)
 
-plt.plot(x,Ps.get_pdf_values(x,0))
+plt.plot(y,Ps.get_cdf_values(y,0))
+plt.plot(y,P.get_cdf_values(y,0))
 # plt.xscale('log')
 plt.show()
 
+sns.kdeplot(Ps.get_samples(0,10000))
+plt.show()
 
 
 
