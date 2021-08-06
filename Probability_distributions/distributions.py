39b1c571c914f626144ef6d1570284e9563e59ce
Anabel del Val <ana.isabel.del.val.benitez@vki.ac.be>
2021-08-06 11:42:39 +0200

    commit

--- Probability_distributions/distributions.py
+++ Probability_distributions/distributions.py
@@ -26,38 +26,54 @@ class Gaussian:
             self.sigma[i] = self.hyperparams[i][1]
 
     # PDF
-    def get_one_pdf_value(self,x,pos): 
-        return np.divide(1,np.sqrt(2*3.141592653589793)*self.sigma[pos])*np.exp(-np.power(np.abs(self.mu[pos] - x),2)/(2*np.power(self.sigma[pos],2)))
+    def get_one_pdf_value(self,x,pos=0): 
+        return np.divide(1,np.sqrt(2*np.pi)*self.sigma[pos])*np.exp(-np.power(np.abs(self.mu[pos] - x),2)/(2*np.power(self.sigma[pos],2)))
 
-    def get_one_prop_logpdf_value(self,x,pos): 
+    def get_one_prop_logpdf_value(self,x,pos=0): 
         return -np.power(np.abs(self.mu[pos] - x),2)/(2*np.power(self.sigma[pos],2))
 
-    def get_pdf_values(self,x,pos):
+    def get_pdf_values(self,x,pos=0):
         values = [0.]*len(x)
         for i in range(len(x)):
             values[i] += self.get_one_pdf_value(x[i],pos)
         return values
 
+    # PDF as function
+    def fun_pdf(self,pos=0):
+        return lambda u: self.get_one_pdf_value(u,pos)
+
+    # LogPDF as function
+    def fun_logpdf(self,pos=0):
+        return lambda u: self.get_one_prop_logpdf_value(u,pos)
+
     # CDF
-    def get_one_cdf_value(self,x,pos): 
+    def get_one_cdf_value(self,x,pos=0): 
         return 0.5*(1+special.erf(np.divide(x-self.mu[pos],self.sigma[pos]*np.sqrt(2))))
 
-    def get_cdf_values(self,x,pos):
+    def get_cdf_values(self,x,pos=0):
         values = [0.]*len(x)
         for i in range(len(x)):
             values[i] += self.get_one_cdf_value(x[i],pos)
         return values
 
-    # Inverse CDF
-    def inv_cdf(self,x,pos):
+    # CDF as function
+    def fun_cdf(self,pos=0):
+        return lambda u: self.get_one_cdf_value(u,pos)
+
+    # Evaluation of inverse CDF in x
+    def inv_cdf(self,x,pos=0):
         return self.sigma[pos]*np.sqrt(2)*special.erfinv((2*x-1))+self.mu[pos]
 
+    # Inverse CDF as function
+    def fun_icdf(self,pos=0):
+        return lambda u: self.inv_cdf(u,pos)
+
     # Generate random samples
-    def get_one_sample(self,pos):
+    def get_one_sample(self,pos=0):
         u = np.random.random()
         return self.inv_cdf(u,pos)
 
-    def get_samples(self,pos,nsamples):
+    def get_samples(self,nsamples,pos=0):
         values = [0.]*nsamples
         for i in range(nsamples):
             values[i] = self.get_one_sample(pos)
@@ -92,20 +108,24 @@ class Uniform:
         # self.hypvol = np.subtract(self.ub,self.lb)
 
     # PDF
-    def get_one_pdf_value(self,x,pos): 
+    def get_one_pdf_value(self,x,pos=0): 
         if x>=self.lb[pos] and x<=self.ub[pos]:
             return np.divide(1,self.ub[pos]-self.lb[pos])
         else:
             return 0.
 
-    def get_pdf_values(self,x,pos):
+    def get_pdf_values(self,x,pos=0):
         values = [0.]*len(x)
         for i in range(len(x)):
             values[i] += self.get_one_pdf_value(x[i],pos)
         return values
 
+    # PDF as function
+    def fun_pdf(self,pos=0):
+        return lambda u: self.get_one_pdf_value(u,pos)
+
     # CDF
-    def get_one_cdf_value(self,x,pos): 
+    def get_one_cdf_value(self,x,pos=0): 
         if x<self.lb[pos]:
             return 0.
         elif x>=self.lb[pos] and x<=self.ub[pos]:
@@ -113,14 +133,14 @@ class Uniform:
         else:
             return 1.
 
-    def get_cdf_values(self,x,pos):
+    def get_cdf_values(self,x,pos=0):
         values = [0.]*len(x)
         for i in range(len(x)):
             values[i] += self.get_one_cdf_value(x[i],pos)
         return values
 
-    # Inverse CDF
-    def inv_cdf(self,x,pos):
+    # Inverse CDF at x
+    def inv_cdf(self,x,pos=0):
         if x<=0.:
             return self.lb[pos]
         elif x>=self.lb[pos] and x<=self.ub[pos]:
@@ -128,12 +148,20 @@ class Uniform:
         else:
             return self.ub[pos]
 
+    # CDF as function
+    def fun_cdf(self,pos=0):
+        return lambda u: self.get_one_cdf_value(u,pos)
+
+    # Inverse CDF as function
+    def fun_icdf(self,pos=0):
+        return lambda u: self.inv_cdf(u,pos)
+
     # Generate random samples
-    def get_one_sample(self,pos):
+    def get_one_sample(self,pos=0):
         u = np.random.random()
         return self.inv_cdf(u,pos)
 
-    def get_samples(self,pos,nsamples):
+    def get_samples(self,nsamples,pos=0):
         values = [0.]*nsamples
         for i in range(nsamples):
             values[i] = self.get_one_sample(pos)
--- examples/pce.py
+++ examples/pce.py
@@ -1,12 +1,24 @@
 import sofia.pc as pce
 import numpy as np
 import matplotlib.pyplot as plt
+import sofia.distributions as dist
 
-# Generate a bunch of Gaussian random variables to use
+"""
+
+        Sampling a uniform distribution with Gaussians using PCE
+
+"""
+
+# Generate Gaussian samples to use
 N = 5000
-S = np.random.normal(loc=0,scale=1,size=N)
+hyp=[[0.,1.]]
+G = dist.Gaussian(1,hyp)
+S = G.get_samples(N)
 
-h = pce.expo_icdf([])
+hypU=[[0.,1.]]
+target = dist.Uniform(1,hypU)
+
+h = target.fun_icdf()
 
 ki_uniform = pce.approximate_rv_coeffs(13, h)
 k = pce.generate_rv(ki_uniform, S)
