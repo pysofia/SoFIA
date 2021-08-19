eab8530470a0559724b5ee51f9000c51751f971d
Anabel del Val <ana.isabel.del.val.benitez@vki.ac.be>
2021-08-19 16:26:29 +0200

    commit

---
+++ Applications/examples/adiabatic_reactor/inference/inference.py
@@ -0,0 +1,238 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sofia.distributions as dist
+import sofia.sampler as mcmc
+from cycler import cycler
+import mutationpp as mpp
+import scipy
+from scipy.optimize import minimize
+
+# Plotting settings
+
+SMALL_SIZE = 12
+MEDIUM_SIZE = 14
+BIGGER_SIZE = 18
+
+plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+cc = cycler('linestyle', ['-', '--', ':', '-.', '-']) * cycler('color', ['r', 'g', 'b', 'm', 'k'])
+plt.rc('axes', prop_cycle=cc)
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='Helvetica')
+
+##
+
+# Time integration
+
+def runge_kutta4(mix, rhoi, T, dt):
+    
+    mix.setState(rhoi,T,0)
+    wdot_1 = mix.netProductionRates() 
+
+    rhoi += 0.5 * np.multiply(dt, wdot_1)
+    mix.setState(rhoi,T,0)
+    wdot_2 = mix.netProductionRates() 
+    
+    rhoi += 0.5 * np.multiply(dt, wdot_2)
+    mix.setState(rhoi,T,0)
+    wdot_3 = mix.netProductionRates() 
+    
+    rhoi += np.multiply(dt,wdot_3)
+    mix.setState(rhoi,T,0)
+    wdot_4 = mix.netProductionRates() 
+
+    return 1./6. * np.multiply(dt, (np.array(wdot_1) + 2 * np.array(wdot_2) + 2 * np.array(wdot_3) + np.array(wdot_4)))
+
+def solver(params):
+
+    # ---------------------------------
+    # Write gsi.xml in mixture.xml files
+
+    with open('/Users/anabel/Documents/PhD/Code/Mutationpp_Michele/Mutationpp/data/mechanisms/air5_Park.xml', 'r') as f:
+        lines = f.readlines()
+
+    # Replace the lines
+    lines[12] = ('{:s}  \n').format('<arrhenius A="'+str(params[0])+'" n="-1.6" T="113200.0" />')  
+    lines[18] = ('{:s}  \n').format('<arrhenius A="'+str(params[1])+'" n="-1.5" T="59360.0" />')  
+    lines[24] = ('{:s}  \n').format('<arrhenius A="'+str(params[2])+'" n="0.0" T="75500.0" />')  
+    lines[30] = ('{:s}  \n').format('<arrhenius A="'+str(params[3])+'" n="0.42" T="42938.0" />')  
+    lines[35] = ('{:s}  \n').format('<arrhenius A="'+str(params[4])+'" n="0.0" T="19400.0" />')  
+    
+
+    # Write the lines back
+    with open('/Users/anabel/Documents/PhD/Code/Mutationpp_Michele/Mutationpp/data/mechanisms/air5_Park.xml', 'w') as f:
+        f.writelines(lines)
+
+    # ## Physico-chemical model settings (mpp)
+    opts = mpp.MixtureOptions("air_5")
+    opts.setThermodynamicDatabase("RRHO")
+    opts.setStateModel("ChemNonEq1T")
+
+    mix = mpp.Mixture(opts)
+    ns = mix.nSpecies()
+    nr = mix.nReactions()
+
+    # ##
+
+    T = 300
+    P = 10000
+    mix.equilibrate(T,P)
+    rhoi_eq = mix.densities()
+
+    Tinit = 15000
+    mix.setState(rhoi_eq,Tinit,1)
+    total_energy = mix.mixtureEnergyMass()*mix.density()
+
+    time = 0.
+    time_final = 1e-7
+    dt = 1.e-9
+    rhoi = rhoi_eq
+
+    y0 = np.divide(rhoi_eq,mix.density())
+    x0 = mix.convert_y_to_x(y0)
+    temperature = np.array(mix.T())
+    temp = [0]*1002
+    # mass_fractions = np.array([y0])
+    # mole_fraction =  np.array([x0])
+    # time_reaction = np.array(time)
+
+    while (time < time_final):
+        dt = min(dt * 1.0002, 1.e-10)
+        drhoi = runge_kutta4(mix, rhoi, total_energy, dt)
+        rhoi += drhoi
+        time += dt
+        mix.setState(rhoi,total_energy,0)
+        y = mix.Y()
+        x = mix.convert_y_to_x(np.array(y))
+        # mass_fractions = np.vstack((mass_fractions,[y]))
+        # mole_fraction =  np.vstack((mole_fraction,[x]))
+        temperature = np.vstack((temperature,mix.T()))
+        # time_reaction  = np.vstack((time_reaction,time))
+
+    for i in range(len(temp)):
+        temp[i] += temperature[i][0]
+
+    return temp
+
+time_steps = 1002
+time = [0]*time_steps
+time_final = 1.e-7
+for i in range(time_steps):
+    time[i] += (i+1)*np.divide(time_final,time_steps)
+
+## Prior distributions ##
+hyp = [[20.,22.],[20.,22.],[10.,15.],[10.,15.],[10.,15.]]
+prior = dist.Uniform(5,hyp)
+
+## Data consist of different temperature readings at different time steps ##
+
+sigma = 100.
+n_obs=1
+true_params = [np.power(10,prior.get_one_sample()),np.power(10,prior.get_one_sample(pos=1)),np.power(10,prior.get_one_sample(pos=2)),np.power(10,prior.get_one_sample(pos=3)),np.power(10,prior.get_one_sample(pos=4))]
+print(np.log10(true_params))
+
+output = solver(true_params)
+
+t_obs = [np.random.randint(0,1003) for i in range(n_obs)]
+time_obs = [time[t_obs[i]] for i in range(n_obs)]
+
+data = [output[t_obs[i]]+np.random.normal(loc=0,scale=sigma) for i in range(n_obs)] # Five random points in time to perform noisy measurement
+
+## Likelihood definition
+h = [[data[i],sigma] for i in range(n_obs)]
+Lik = dist.Gaussian(n_obs,h)
+
+## Log-likelihood function
+def log_likelihood(Xi):
+
+    for i in range(len(Xi)):
+        if Xi[i]<prior.lb[i]:
+            return -1.e16
+        elif Xi[i]>prior.ub[i]:
+            return -1.e16
+
+    s = solver(np.power(10,Xi))
+
+    value = 0.
+    for i in range(n_obs):
+        value += Lik.get_one_prop_logpdf_value(s[t_obs[i]],i)
+
+    return value
+
+## mLog-likelihood function for MAP estimate
+def mlog_likelihood(Xi):
+    return -1.*log_likelihood(Xi)
+
+## Looking for the MAP point to start sampling ##
+par = [21.,21.,11.,11.,11.]
+res = scipy.optimize.minimize(mlog_likelihood,par,method='Nelder-Mead',tol=1e-6)
+print("MAP found at: "+str(res.x))
+
+# MCMC sampling
+sampler = mcmc.metropolis(np.identity(5)*0.01,log_likelihood,1000)
+
+sampler.seed(res.x)
+sampler.Burn()
+
+XMCMC = []
+
+nchain = 1000
+for i in range(nchain):
+    XMCMC.append(sampler.DoStep(1))
+    print('Step: '+ str(i))
+
+XMCMC = np.array(XMCMC)
+
+## Plotting noisy observations and calibrated polynomial model ##
+
+f_mean = [0.]*time_steps
+f_u = [0.]*time_steps
+f_l = [0.]*time_steps
+
+sol = np.zeros((nchain,time_steps))
+
+for i in range(nchain):
+    sol[i] = solver(np.power(10,XMCMC[i]))
+
+for j in range(time_steps):
+    f_mean[j] = np.mean(sol[:,j])
+    f_u[j] = np.percentile(sol[:,j],97.5)
+    f_l[j] = np.percentile(sol[:,j],2.5)
+
+plt.scatter(time_obs,data,color='black',label='Observations')
+plt.plot(time,f_mean,color='green',label='Mean')
+plt.plot(time,output,color='red',label='True model')
+plt.fill_between(time,f_l,f_u,color='green',alpha=0.2,label='95\% C. I.')
+plt.plot(time,f_u,linestyle='--', color='green')
+plt.plot(time,f_l,linestyle='--', color='green')
+ax = plt.gca()
+ax.yaxis.set_label_coords(0.0, 1.00)
+plt.ylabel('Temperature, K', rotation=0)
+plt.xlabel('time, s')
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False) 
+plt.xlim(0.,np.power(10.,-7.))
+plt.legend() 
+plt.show()
+
+## Kernel plotting
+
+dict_var = {0: 'N2+M=2N+M',1:'O2+M=2O+M',2:'NO+M=N+O+M',3:'N2+O=NO+N',4:'NO+O=O2+N'} # The way they are positioned in MCMC chain
+
+for i in range(5):
+    sns.kdeplot(XMCMC[:,i],label=dict_var[i],shade=True)
+
+plt.legend()
+plt.show()
+
+## Chain diagnostics
+
+var = [0,1,2,3,4] # Position in XMCMC chain
+
+sampler_diag = mcmc.diagnostics(XMCMC,dict_var)
+sampler_diag.chain_visual(5,var,1000) # 5 plots, show 5000 samples
+sampler_diag.autocorr(70,5,var) # 70 lags
--- Applications/examples/adiabatic_reactor/propagation/ci.py
+++ Applications/examples/adiabatic_reactor/propagation/ci.py
@@ -29,9 +29,11 @@ for i in range(time_steps):
     time[i] += (i+1)*np.divide(time_final,time_steps)
 
 func = np.zeros((N,time_steps))
+func2 = np.zeros((N,time_steps))
 
 for j in range(N):
-    func[j] = np.loadtxt('./output/temp_profile_p1atm_'+str(j)+'.dat')
+    func[j] = np.loadtxt('./output/temp_profile_'+str(j)+'.dat')
+    func2[j] = np.loadtxt('./output/temp_profile_p15_'+str(j)+'.dat')
 
 ## 95 % C.I. ##
 
@@ -39,24 +41,39 @@ mean = [0.]*time_steps
 lower = [0.]*time_steps
 upper = [0.]*time_steps
 
+mean2 = [0.]*time_steps
+lower2 = [0.]*time_steps
+upper2 = [0.]*time_steps
+
 for i in range(time_steps):
     m = [0.]*N
+    m2 = [0.]*N
     for j in range(N):
         m[j] = func[j][i]
+        m2[j] = func2[j][i]
 
     mean[i] = np.mean(m)
     upper[i] = np.percentile(m,97.5)
     lower[i] = np.percentile(m,2.5)
+
+    mean2[i] = np.mean(m2)
+    upper2[i] = np.percentile(m2,97.5)
+    lower2[i] = np.percentile(m2,2.5)
 
 labels_T = {0: 'N2+M=2N+M', 1: 'O2+M=2O+M', 2: 'NO+M=N+O+M', 3: 'N2+O=NO+N', 4: 'NO+O=O2+N'}
 colors = {0: 'green', 1: 'red', 2: 'blue', 3: 'orange', 4: 'pink'}
 
 plt.figure()
 
-plt.plot(time,mean,label='mean',color=colors[0])
+plt.plot(time,mean,label='100hPa',color=colors[0])
 plt.plot(time,upper,linestyle='--',color=colors[0])
 plt.plot(time,lower,linestyle='--',color=colors[0])
 plt.fill_between(time,lower,upper,color=colors[0],alpha=0.2,label='95\% C.I.')
+
+plt.plot(time,mean2,label='15hPa',color=colors[1])
+plt.plot(time,upper2,linestyle='--',color=colors[1])
+plt.plot(time,lower2,linestyle='--',color=colors[1])
+plt.fill_between(time,lower2,upper2,color=colors[1],alpha=0.2,label='95\% C.I.')
 
 ax = plt.gca()
 ax.yaxis.set_label_coords(0.0, 1.00)
@@ -64,6 +81,7 @@ plt.ylabel('Temperature, K', rotation=0)
 plt.xlabel('time, s')
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False) 
-plt.ylim(6000., 15500.)
+plt.xlim(0.,np.power(10.,-7.))
+# plt.ylim(6000., 15500.)
 plt.legend() 
 plt.show()
